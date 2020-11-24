ee8fd3995d857cedde79bd0a95fed12481eb1867
Hstov <hstoven@hotmail.fr>
2020-11-24 14:16:42 +0100

    a few changes to fix the application so it can run

--- initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -1,14 +1,12 @@
 package com.example.securingweb;
 
-import org.springframework.context.annotation.Bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -27,34 +25,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/perform_login")
                 .defaultSuccessUrl("/homepage.html", true)
                 .failureUrl("/login.html?error=true")
-                .failureHandler(authenticationFailureHandler())
+          //      .failureHandler(authenticationFailureHandler())
                 .and()
                 .logout()
                 .logoutUrl("/perform_logout")
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessHandler(logoutSuccessHandler());
+                .deleteCookies("JSESSIONID");
+           //     .logoutSuccessHandler(logoutSuccessHandler());
     }
 
-    private LogoutSuccessHandler logoutSuccessHandler() {
+   //@Autowired
+   // private LogoutSuccessHandler logoutSuccessHandler() {
 		// TODO Auto-generated method stub
-		return null;
-	}
-
+    //	return new CustomLogoutSuccessHandler();
+	//	return null;
+	//}
+    
+    @Autowired
 	public AuthenticationFailureHandler authenticationFailureHandler() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER","ADMIN");
     }
+    
 }
