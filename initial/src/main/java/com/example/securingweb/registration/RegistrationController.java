62d36be541022278a127e24321c656ba7de3c10e
Hstov <hstoven@hotmail.fr>
2020-11-27 14:24:52 +0100

    registration controller update

--- initial/src/main/java/com/example/securingweb/registration/RegistrationController.java
+++ initial/src/main/java/com/example/securingweb/registration/RegistrationController.java
@@ -9,8 +9,12 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.securingweb.exceptions.UserAlreadyExistException;
 import com.example.securingweb.persistence.model.User;
@@ -31,12 +35,36 @@ public class RegistrationController {
             HttpServletRequest request, Errors errors) {
 
     }*/
+    
+    @ModelAttribute(value = "myEntity")
+    public UserDto newEntity()
+    {
+        return new UserDto();
+    }
+    
+    @RequestMapping(value = "/successRegister", method = RequestMethod.POST)
+    public View action(Model model, @ModelAttribute("myEntity") User myEntity)
+    {
+        // save the entity or do whatever you need
 
+        return new RedirectView("/user/ranks");
+    }
+    
+    @RequestMapping(value = "/successRegister", method = RequestMethod.GET)
+    public ModelAndView getRanks(Model model, HttpServletRequest request)
+    {
+        String view = "the-view-name";
+        return new ModelAndView(view, "command", model);
+    }
+    
+    
     @PostMapping("/register")
     public ModelAndView registerUserAccount(
             @ModelAttribute("user") @Valid UserDto userDto,
             HttpServletRequest request, Errors errors) {
-
+    		
+    	
+    	System.out.println("On est passé par là!");
        /* try {
             User registered = userService.registerNewUserAccount(userDto);
         } catch (UserAlreadyExistException uaeEx) {
